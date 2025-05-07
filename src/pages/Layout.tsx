4cdfe2e614a0ce451e11d7ed4bfc910815faf29e
Paul Dias <pdias@brightcove.com>
2025-05-07 15:21:42 -0400

    simplify Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,14 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Nav from "../components/Nav";
 
-import { routes } from "../utils/routes";
-
 const Layout: React.FC = () => {
-    const paths = location.pathname.split("/");
-    const basePathname = `/${paths[1]}`;
-    console.log(basePathname);
-    const defaultSelected = routes.get(basePathname)?.name || "Home";
-
     // Create a client
     const queryClient = new QueryClient();
 
