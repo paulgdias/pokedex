05ac201c4d421491712702adce3b320efe546523
Paul Dias <pdias@brightcove.com>
2025-06-01 20:33:26 -0400

    styling

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC = () => {
     return (
         <div className="flex flex-row h-dvh">
             <Nav />
-            <div className={`page w-screen m-4 overflow-y-hidden"}`}>
+            <div className="page w-screen m-4 overflow-y-hidden">
                 <ErrorBoundary
                     fallback={
                         <div>
